4e5fecbefd9e7304c9be10a40635823acf25fa8b
SpiderDead <mimij68@live.nl>
2020-10-05 13:40:21 +0200

    Fixed price formatting when price >= 1000

--- src/Service/PaymentMethodService.php
+++ src/Service/PaymentMethodService.php
@@ -260,7 +260,7 @@ class PaymentMethodService
         $orderReference = '',
         $cardToken = false
     ) {
-        $amount = number_format($amount, 2);
+        $amount = number_format($amount, 2, '.', '');
         if (!$orderReference) {
             $this->module->currentOrderReference = $orderReference = Order::generateReference();
         }
